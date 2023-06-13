ae3861f7c141e52a112dd783b04876d9a50a7f2c
bobbybarton3 <bobbybarton3@gmail.com>
2023-06-13 13:42:21 -0500

    added prototype assets for book and shelf

---
+++ bartonhat-shop/src/components/Book.svelte
@@ -0,0 +1,15 @@
+<div class="book1">
+</div>
+
+
+
+<style lang="scss">
+    .book1{
+        height: inherit;
+        background-image: url("Book1.png");
+        background-position-y: center;
+        min-height: 32%;
+        background-repeat: no-repeat;
+    }
+
+</style>
---
+++ bartonhat-shop/src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+    import Book from "../components/Book.svelte";
+
+
+
+
+</script>
+
+<div class="background">
+    <Book/>
+</div>
+
+<style lang="scss">
+    .background{   
+        position: absolute;
+        overflow:hidden;
+        background-image: url("bookshelf-backgroundBHC2.jpg");
+        background-repeat: no-repeat;
+        background-size:cover;
+        height: 100vh;
+        width: 100vw;
+    }
+
+    
+</style>
